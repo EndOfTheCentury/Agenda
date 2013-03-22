0c6473fd5e769d9e43b7ad1a6d6ff14b6c0c101e
guyen <guyen@192.168.1.128>
2013-03-22 20:20:55 +0100

    Cambios en la ventana

--- src/agenda/VentanaInicial.java
+++ src/agenda/VentanaInicial.java
@@ -10,21 +10,41 @@ package agenda;
  */
 import com.toedter.calendar.JCalendar;
 import com.toedter.calendar.demo.DateChooserPanel;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.TimeZone;
-public class VentanaInicial {
-
+import javax.swing.JFrame;
+public class VentanaInicial extends JFrame {
+    
+    private JCalendar calendario;
+    
+    public VentanaInicial(){
+        setTitle("Calendario");
+        setName("VentanaInicial");
+        Container contenedor =  getContentPane();
+        contenedor.setLayout(null);
+        
+        calendario = new JCalendar(new GregorianCalendar(TimeZone.getDefault()));
+        calendario.setSize(300,250);
+        calendario.setLocation(0,0);
+        calendario.setVisible(true);
+        contenedor.add(calendario);
+        
+        Toolkit tool = getToolkit();
+        Dimension d = tool.getScreenSize();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(d.width/3, d.height/2);
+        setLocation((d.width/2)-(getWidth()/2),(d.height/2)-(getHeight()/2));      
+        setVisible(true);
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        Calendar c =new GregorianCalendar(TimeZone.getDefault());
-        JCalendar cal = new JCalendar();
-        DateChooserPanel dcp = new DateChooserPanel();
        
-
-         
+        VentanaInicial ventana=new VentanaInicial();
     }
 }
